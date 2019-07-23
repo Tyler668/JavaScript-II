6db23cb92b0f762f040a0c46ee168c090007abb7
Tyler Gilchrist <tyler668@gmail.com>
2019-07-23 16:34:28 -0400

    Finished an example closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function Door1(){
+  function Door2(){
+    var secret = "There is no secret";
+    console.log(secret);
+  }
+  Door2();
+}
+Door1();
+
+
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
